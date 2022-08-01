a48b13f65c22107ca047473636345bf0ff4469ec
John Flynn <johnhfflynn25@gmail.com>
2022-08-01 13:38:53 +1200

    Create Unreal project

---
+++ GhostOfDeath/Source/GhostOfDeath/GhostOfDeathGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GhostOfDeathGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GHOSTOFDEATH_API AGhostOfDeathGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
